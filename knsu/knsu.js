cb7142f34df92d9ae96aa84cfe120d273cff7b27
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-12-24 12:49:46 +0900

    追加

---
+++ knsu/knsu.js
@@ -0,0 +1,23 @@
+
+
+// nameIntro("yukiho")
+
+(function nameIntro(x){
+    
+    let myName = x;
+    
+    console.log("私の名前は"+myName+"です")
+    
+}
+("透歩")
+);
+
+// nameIntro("yuki");
+
+
+// nameIntro("yukiho")
+
+// nameIntro = function(x){
+//     let myName = x;
+//     console.log("私の名前は"+myName+"です");
+// }
