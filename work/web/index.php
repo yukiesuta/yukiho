f79888ac1018c32e47a47f94c09782478d179766
“yukiesuta” <matsumoto.yukiho.0704@gmail.com>
2021-12-30 13:00:23 +0900

    commit

--- work/web/index.php
+++ work/web/index.php
@@ -7,8 +7,9 @@ require('../app/_parts/_header.php');
 ?>
 
 <form action="result.php" method="get">
-  <input type="text" name="message">
-  <input type="text" name="username">
+  <!-- <input type="text" name="message">
+  <input type="text" name="username"> -->
+  <textarea name="message" id="" cols="30" rows="10"></textarea>
   <button>Send</button>
 </form>
 
